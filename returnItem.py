3264b2c278e4e695b3f5088a9a9b76383dafdc13
pdemetci <pinardemetci@gmail.com>
2015-12-16 23:15:07 -0500

    refined returnItem code

--- returnItem.py
+++ returnItem.py
@@ -2,26 +2,24 @@ from weatherForecastInfo import WeatherForecast
 import serial
 import time
 import random
-import GUI2
+import returnItemGUI
 
 class returnItem():	
+	"""
+	This class handles the communication to the serial port while returning an item.
+	returnItemGUI implements its state depending on the mouse action on the GUI.
+	"""
 	def __init__ (self):
-		self.weatherForecast = WeatherForecast(api_key= "18e78bb19491e1929765a1354c99d45a", lat=42.27453, lng=-71.243861)
-		self.weatherForecastInfo = self.weatherForecast.getWeatherForecast()
 		self.status = None	# status of table: 0 - default / 1- umbrella / 2 - cardigan / 3 - sun cap / 4 - scarf
 		self.ser=serial.Serial("/dev/ttyACM1", 9600, timeout=5)
-		self.gui=GUI2.returnGUI()
+		self.gui=returnItemGUI.returnGUI()
 
 	def mainTable(self):
 		self.communicateToSerial()
 
 	def communicateToSerial(self):
-		ser = serial.Serial(port = "/dev/ttyACM1", baudrate=9600)
-		ser.close()
-		ser.open()
-		random.seed()
-		while ser.isOpen():
-			ser.write(self.gui.state)
+		if self.ser.isOpen():
+			self.ser.write(self.gui.state)
 			time.sleep(1)
 			
 
